1f4db0f59db9c75527880daa368b405dbfbd3c04
Bart van den Berkmortel <b.vandenberkmortel@student.fontys.nl>
2018-05-28 17:22:51 +0200

    Bestand voorzien van comments.

--- js/app.js
+++ js/app.js
@@ -8,22 +8,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
   WijzigLettertype();
 });
 
-// Stem module
+// Variabelen uit var.js inladen
 var stemspecs = new stemvariabel();
 
+// Stem module
 var stemmodule = (function() {
 
+  // Variabel aanmaken met het aantal stemmen
   var stemmen = stemspecs.stemmen;
 
+  // Functie om elke keer nadat er geklikt wordt een stem toe te voegen aan het variabel.
   function stem() {
     stemmen += 1;
     updateStemmen();
   }
 
+  // Functie voor het bijwerken van het aantal stemmen.
   function updateStemmen() {
     document.getElementById('stemmen').innerHTML = stemmen;
   }
 
+  // Initialiseren van het aantal huidigen stemmen en het uitvoeren van de functie wanneer er op de knop geklikt wordt.
   var init = function() {
     console.log('init');
     document.getElementById('stem').addEventListener('click', stem);
