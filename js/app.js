ea9c09db7c031168259daed44294c12e5c68ea59
Bart van den Berkmortel <b.vandenberkmortel@student.fontys.nl>
2018-06-11 19:56:16 +0200

    1 staaf

--- js/app.js
+++ js/app.js
@@ -13,10 +13,10 @@ var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Red"],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
